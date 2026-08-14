2249cfc7c3c2a1f3a09918509bd1157f9e1f322e
toypajnv <vjt9wnv248@privaterelay.appleid.com>
2026-08-14 23:33:43 +0500

    Add Android API interface

---
+++ android/app/src/main/java/ru/rpo/mobile/data/RpoApi.kt
@@ -0,0 +1,15 @@
+package ru.rpo.mobile.data
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface RpoApi {
+    @POST("api/mobile/events")
+    suspend fun sendEvent(@Body body: EventRequest): Response<EventResponse>
+
+    @GET("api/mobile/events")
+    suspend fun history(@Query("device_id") deviceId: String, @Query("limit") limit: Int = 30): Response<List<EventResponse>>
+}
