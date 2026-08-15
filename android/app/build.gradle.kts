7521f6a55e51d7af6c3007b843e7da048dc44851
toypajnv <vjt9wnv248@privaterelay.appleid.com>
2026-08-15 18:15:49 +0500

    Bump Android test build to 1.1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "ru.rpo.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.2"
         buildConfigField("String", "SERVER_URL", "\"$serverUrl\"")
     }
 
