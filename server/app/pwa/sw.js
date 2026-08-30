977c1363b23ce3044ac750cbed23deb99940059a
toypajnv <vjt9wnv248@privaterelay.appleid.com>
2026-08-30 16:41:24 +0500

    Cache PWA 1.1 UX assets

--- server/app/pwa/sw.js
+++ server/app/pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE='rpo-pwa-shell-v1.0.1';
-const SHELL=['/app/','/app/manifest.webmanifest','/app/icon-180.png','/app/icon-192.png','/app/icon-512.png','/pwa-assets/app.css?v=20260829-2','/pwa-assets/app.js?v=20260829-2'];
+const CACHE='rpo-pwa-shell-v1.1.0';
+const SHELL=['/app/','/app/manifest.webmanifest','/app/icon-180.png','/app/icon-192.png','/app/icon-512.png','/pwa-assets/app.css?v=20260829-2','/pwa-assets/ux.css?v=20260830-1','/pwa-assets/app.js?v=20260829-2','/pwa-assets/ux.js?v=20260830-1'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
