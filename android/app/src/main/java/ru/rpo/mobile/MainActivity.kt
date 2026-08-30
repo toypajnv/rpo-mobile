a14b896748f746386e2cec6613af12e73d90df84
toypajnv <vjt9wnv248@privaterelay.appleid.com>
2026-08-30 16:35:02 +0500

    Switch Android entry point to permit-centric UX

--- android/app/src/main/java/ru/rpo/mobile/MainActivity.kt
+++ android/app/src/main/java/ru/rpo/mobile/MainActivity.kt
@@ -3,11 +3,11 @@ package ru.rpo.mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.rpo.mobile.ui.RpoApp
+import ru.rpo.mobile.ui.RpoUxApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { RpoApp() }
+        setContent { RpoUxApp() }
     }
 }
