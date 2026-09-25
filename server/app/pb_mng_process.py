9a231ef8506643de8cc201a87e12e6fed04c1fbf
toypajnv <vjt9wnv248@privaterelay.appleid.com>
2026-09-25 17:48:32 +0500

    Bump PB_MNG coordinator UX version

--- server/app/pb_mng_process.py
+++ server/app/pb_mng_process.py
@@ -547,7 +547,7 @@ def coordinator_page(request: Request, db: Session = Depends(get_db)):
     response = templates.TemplateResponse(request=request, name="pb_mng_coordinator.html", context={"operator": operator})
     response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
     response.headers["Pragma"] = "no-cache"
-    response.headers["X-PB-MNG-Coordinator-Version"] = "20260925-6"
+    response.headers["X-PB-MNG-Coordinator-Version"] = "20260925-8"
     return response
 
 
